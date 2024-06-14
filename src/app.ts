e4fc055864d6f6e43ae7537724aa659c60556e4a
Mateus <msouzacrv@gmail.com>
2024-06-14 08:48:53 -0300

    :sparkles: feat: handler erros globais

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (env.NODE_ENV !== "prod") {
+    console.error(error);
+  }
+
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: "Validation error.", issues: error.format() });
+  }
+
+  return reply.status(500).send({ message: "Internal server error." });
+});
