d91aba299dec0281558b44cef049027872d5e470
Mateus <msouzacrv@gmail.com>
2024-06-14 08:34:57 -0300

    :sparkles: feat: lidando com exceções

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,8 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
 import { RegisterUseCase } from "../../use-cases/register";
-import { PrismaUsersRepository } from "../../repositories/prisma-users-repository";
-import { InMemoryUsersRepository } from "../../repositories/in-memory-users-repository";
+import { PrismaUsersRepository } from "../../repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "../../use-cases/errors/user-already-exists-error";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -14,7 +14,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    const usersRepository = new InMemoryUsersRepository();
+    const usersRepository = new PrismaUsersRepository();
     const registerUseCase = new RegisterUseCase(usersRepository);
 
     await registerUseCase.execute({
@@ -23,7 +23,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     });
   } catch (err) {
-    return reply.status(409).send();
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message });
+    }
+
+    return reply.status(500).send(); // TODO: fix me
   }
 
   return reply.status(201).send();
