045f5b30444ff627e4d14fe8e6149925a2759016
Mateus <msouzacrv@gmail.com>
2024-06-18 09:03:38 -0300

    :sparkles: feat: criando case de buscar usuário por id

--- src/repositories/in-memory/in-memory-repository.ts
+++ src/repositories/in-memory/in-memory-repository.ts
@@ -1,9 +1,18 @@
 import { Prisma, User } from "@prisma/client";
-import { prisma } from "../../lib/prisma";
 import { UsersRepository } from "../users-repository";
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = [];
+
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id);
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
 
   async findByEmail(email: string) {
     const user = this.items.find((item) => item.email === email);
