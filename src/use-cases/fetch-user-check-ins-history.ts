b81e49b5c90de8871a9224c6b7f24748d1ac67db
Mateus <msouzacrv@gmail.com>
2024-06-19 09:50:48 -0300

    :sparkles: feat: historico de check-ins do usuário

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,29 @@
+import { CheckIn } from "@prisma/client";
+import { CheckInsRepository } from "../repositories/check-ins-repository";
+
+interface FetchUserCheckInHistoryUseCaseRequest {
+  userId: string;
+  page: number;
+}
+
+interface FetchUserCheckInHistoryUseCaseResponse {
+  checkIns: CheckIn[];
+}
+
+export class FetchUserCheckInHistoryUseCase {
+  constructor(private checkInRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInHistoryUseCaseRequest): Promise<FetchUserCheckInHistoryUseCaseResponse> {
+    const checkIns = await this.checkInRepository.findManyByUserId(
+      userId,
+      page
+    );
+
+    return {
+      checkIns,
+    };
+  }
+}
