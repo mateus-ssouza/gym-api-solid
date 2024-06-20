342024515f7ef7bdc5dd85e6f3717e6db50e1440
Mateus <msouzacrv@gmail.com>
2024-06-20 11:10:14 -0300

    :sparkles: feat: rota validate check ins

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -3,6 +3,7 @@ import { verifyJWT } from "../../middlewares/verify-jwt";
 import { create } from "./create";
 import { history } from "./history";
 import { metrics } from "./metrics";
+import { validate } from "./validate";
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
@@ -10,4 +11,5 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.post("/gyms/:gymId/check-ins", create);
   app.get("/check-ins/history", history);
   app.get("/check-ins/metrics", metrics);
+  app.patch("/check-ins/:checkInId/validade", validate);
 }
