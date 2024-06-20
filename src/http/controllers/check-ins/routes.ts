1ab92003a52d3f642d285b2c64168dc63fe60db4
Mateus <msouzacrv@gmail.com>
2024-06-20 11:08:11 -0300

    :sparkles: feat: rota history check ins

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../../middlewares/verify-jwt";
 import { create } from "./create";
+import { history } from "./history";
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
   app.post("/gyms/:gymId/check-ins", create);
+  app.get("/check-ins/history", history);
 }
