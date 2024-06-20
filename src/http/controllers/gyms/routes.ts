9ee4499eba441a57b0ea382186bb7910f5a381af
Mateus <msouzacrv@gmail.com>
2024-06-20 11:00:02 -0300

    :sparkles: feat: rota nearby gyms

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,10 +2,12 @@ import { FastifyInstance } from "fastify";
 import { verifyJWT } from "../../middlewares/verify-jwt";
 import { create } from "./create";
 import { search } from "./search";
+import { nearby } from "./nearby";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJWT);
 
   app.post("/gyms", create);
   app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
 }
