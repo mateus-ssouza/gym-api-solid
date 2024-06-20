3f9b592cf25d9f123cabdcb66422f5aec66f17a6
Mateus <msouzacrv@gmail.com>
2024-06-20 10:56:27 -0300

    :truck: chore: movendo arquivos para nova pasta users na controllers

--- src/http/controllers/users/profile.spec.ts
+++ src/http/controllers/users/profile.spec.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import { app } from "../../app";
+import { app } from "../../../app";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
 describe("Profile User Controller (e2e)", () => {
